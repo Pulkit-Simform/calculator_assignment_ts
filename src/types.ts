89fc0e1c26e88a36e3808f9497f965d27bd6e889
Pulkit Sharma <pulkit@simformsolutions.com>
2023-05-03 17:15:21 +0530

    Keyboard events added

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface GlobalObjectAssignment{
+    numberArray:Array<string>
+    arithmeticOperatorArray: Array<string>
+    equalOperator:Array<string>
+    clearOperator:Array<string>
+    miscOperator:Array<string>
+    computeOperator:Array<string>
+}
+
+export interface bindStringObjectAssignment{
+    dot:string 
+    left_parentheses:string
+    right_parentheses:string
+    pi:string
+    exponential:string
+    plus_minus:string
+    raiseTo:string
+}
+
+
